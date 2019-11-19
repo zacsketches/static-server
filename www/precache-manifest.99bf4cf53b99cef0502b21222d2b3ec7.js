97b3e29b733858ebc78e0fc14d1def532f80c102
Zac Staples <zac@fathom5.co>
2019-11-19 18:10:10 -0500

    initial commit

---
+++ www/precache-manifest.99bf4cf53b99cef0502b21222d2b3ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c7d1ac7a2d21989f6ac8d030957d40",
+    "url": "/melonunderground.github.io/fire-main/index.html"
+  },
+  {
+    "revision": "8971ba29581f9809982d",
+    "url": "/melonunderground.github.io/fire-main/static/css/2.07acf49a.chunk.css"
+  },
+  {
+    "revision": "fb64cd3955fb632fbcaa",
+    "url": "/melonunderground.github.io/fire-main/static/css/main.dc95557e.chunk.css"
+  },
+  {
+    "revision": "8971ba29581f9809982d",
+    "url": "/melonunderground.github.io/fire-main/static/js/2.51ac6a2c.chunk.js"
+  },
+  {
+    "revision": "fb64cd3955fb632fbcaa",
+    "url": "/melonunderground.github.io/fire-main/static/js/main.d14b23db.chunk.js"
+  },
+  {
+    "revision": "089a28f54c19b4b754a1",
+    "url": "/melonunderground.github.io/fire-main/static/js/runtime-main.363a6e5d.js"
+  },
+  {
+    "revision": "09a8f8b3b6f80e1ff72df5bf4c518e7b",
+    "url": "/melonunderground.github.io/fire-main/static/media/firemainremove.09a8f8b3.png"
+  }
+]);
